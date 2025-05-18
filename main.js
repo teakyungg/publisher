9339bcbed5d0e9910df6797fec23f40d999adad8
teakyungg <kak0502@naver.com>
2025-05-19 04:13:02 +0900

    feat/header-ani-new

--- main.js
+++ main.js
@@ -9,8 +9,9 @@
         // name 글자 애니메이션
         const name_box = header.querySelector('.name');
         const t1 = gsap.timeline();
-        t1.to(name_box,{opacity: 1, duration: 0.5, scale: 2});
-        t1.to(name_box,{duration: 0.5, scale: 1 ,x:'+=-150', y:-150});
+        t1.set(name_box,{opacity: 0});
+        t1.to(name_box,{duration:2 , opacity:1});
+
 
         // Publisher 글자 애니메이션
         const publisher_box = header.querySelector('.publisher');
@@ -28,14 +29,14 @@
 
         const spans = publisher_box.querySelectorAll('span');
         const t2 = gsap.timeline({delay: 1});
-        t2.set(spans,{y:200})
-        t2.to(spans,{duration:1, opacity:1, y:0, stagger: 0.1})
+        t2.to(spans,{duration:1, opacity:1, stagger: 0.1})
 
         // Portfolio 글자 애니메이션
         const portfolio_box = header.querySelector('.portfolio');
         const t3 = gsap.timeline({delay:2});
-        t3.to(portfolio_box, {x:-1000, y:100, scale:1.5})
-        t3.to(portfolio_box, { duration:1, opacity:1 ,x:100, ease: "power1.inOut"})
+        t3.set(portfolio_box, {x:-1000});
+        t3.to(portfolio_box, {duration:2, opacity:1, x:0, ease:'expo.out'});
+        
     })
 
       
