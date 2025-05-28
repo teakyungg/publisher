f590610641dd449f6137ea7e93bec7001770012a
teakyungg <kak0502@naver.com>
2025-05-29 02:41:00 +0900

    feat/intro-message slogan ani

--- section/intro-message.js
+++ section/intro-message.js
@@ -2,22 +2,61 @@
   const intro = document.querySelector(".intro-message");
   const title = intro.querySelector(".intro-title");
 
+  const slogan_txt_01 = intro.querySelector(".slogan-txt:nth-child(1)");
+  const slogan_txt_02 = intro.querySelector(".slogan-txt:nth-child(2)");
+  const slogan_txt_03 = intro.querySelector(".slogan-txt:nth-child(3)");
+
+  let delay = false;
+
   window.addEventListener("load", () => {
     gsap.to(intro, {
       scrollTrigger: {
         trigger: intro,
-        start: "top 60%",
+        start: "top 50%",
+        // marks: true,
 
         onEnter: () => {
-          title.classList.toggle("animate__animated");
-          title.classList.toggle("animate__rubberBand");
+          sloganAni();
         },
 
         onLeaveBack: () => {
-          title.classList.toggle("animate__animated");
-          title.classList.toggle("animate__rubberBand");
+          sloganAni();
         },
       },
     });
   });
+
+  // 슬로건 애니메이션
+  function sloganAni() {
+    // 딜레이 세팅
+    if (delay) return;
+    setDelay();
+
+    // 목표
+    title.classList.toggle("animate__animated");
+    title.classList.toggle("animate__rubberBand");
+
+    // Trying
+    slogan_txt_01.classList.toggle("animate__animated");
+    slogan_txt_01.classList.toggle("animate__bounce");
+
+    // Learning
+    setTimeout(() => {
+      slogan_txt_02.classList.toggle("animate__animated");
+      slogan_txt_02.classList.toggle("animate__bounce");
+    }, 500);
+
+    // Growing
+    setTimeout(() => {
+      slogan_txt_03.classList.toggle("animate__animated");
+      slogan_txt_03.classList.toggle("animate__bounce");
+    }, 1000);
+  }
+
+  // 딜레이 컨트롤
+  function setDelay() {
+    setTimeout(() => {
+      delay = false;
+    }, 1500);
+  }
 }
