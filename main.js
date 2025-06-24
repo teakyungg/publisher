a71a8c0ed4e0a7a1aa78f306169dd8fa7394892d
teakyungg <kak0502@naver.com>
2025-06-25 00:02:03 +0900

    restart

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 import "./pages/index/header/header.js";
 import "./pages/index/intro/intro-message.js";
-import "./pages/index/project/starbucks.js";
-import "./pages/index/project/2048_game.js";
-import "./pages/index/project/karly.js";
 import "./pages/index/nav/nav-scroll.js";
